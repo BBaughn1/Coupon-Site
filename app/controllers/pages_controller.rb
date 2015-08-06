6186a1bee4c8d585602e8d117e68197e9162b618
Brendon Baughn <Brendon@Marions-MacBook-Pro.local>
2015-08-05 22:58:29 -0400

    My CouponSite
    Something I’m creating for my mother

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+
+	before_action :require_user, only: [:index, :show]
+
+	def index
+		@coupons = Coupon.all
+	end
+
+	def show
+		@coupon = Coupon.find(params[:id])
+		@photos = @coupon.photos
+	end
+	
+end
---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+heather = User.create(first_name: "Heather", lastname: "Baughn", email: 'heathercpn@hotmail.com', password: 'Outfitter02', password_confirmation: 'baughn1')
